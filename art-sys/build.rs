fdfab17a948d23422490728b2c886f358b790b86
Newton Ni <nwtnni@gmail.com>
2025-11-17 11:41:18 -0600

    Build directly using cc

--- art-sys/build.rs
+++ art-sys/build.rs
@@ -1,20 +1,30 @@
+const ROOT: &str = env!("CARGO_MANIFEST_DIR");
+
 fn main() {
-    let root = env!("CARGO_MANIFEST_DIR");
+    let files = [
+        format!("{ROOT}/../ROWEX/Tree.cpp"),
+        format!("{ROOT}/cpp/rowex.cpp"),
+    ];
+
+    let includes = [
+        String::from("include"),
+        format!("{ROOT}/../ROWEX/"),
+        format!("{ROOT}/../"),
+    ];
 
     cxx_build::bridge("src/lib.rs")
         .cpp(true)
-        .file("cpp/rowex.cpp")
-        .include("include")
-        .include(format!("{root}/../"))
+        .std("c++14")
+        .files(&files)
+        .includes(&includes)
+        .flag("-march=native")
         .compile("art-sys");
-
-    println!("cargo:rustc-link-search=native={root}/../build");
-    println!("cargo:rustc-link-lib=static=ARTSynchronized");
 
     // Must be after linking against `ARTSynchronized`
     pkg_config::probe_library("mimalloc").expect("Could not find mimalloc");
     pkg_config::probe_library("tbb").expect("Could not find tbb");
 
-    println!("cargo:rerun-if-changed={root}/cpp/rowex.cpp");
-    println!("cargo:rerun-if-changed={root}/include/rowex.h");
+    for path in files.iter().chain(&includes) {
+        println!("cargo:rerun-if-changed={path}");
+    }
 }
